52ca5f8f74c84cd9cbde399cc9ed89a2d938837d
MichaelMGonzalez <mmg005@ucsd.edu>
2015-09-26 15:46:22 -0700

    Added FPS controller and floor

---
+++ SDHacksDemo/Assets/Scripts/PlaceBlock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
